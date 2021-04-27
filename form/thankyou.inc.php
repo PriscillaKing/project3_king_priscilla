3c605276bbc3bb4037ea1f7c3405247c0e7fc19a
Priscilla King <pking1@ufl.edu>
2021-04-26 19:30:15 -0700

    Contact Page styling

--- form/thankyou.inc.php
+++ form/thankyou.inc.php
@@ -124,7 +124,7 @@
 </header>
 
 <div class="container">
-  <h1>Thank you for your enquiry</h1>
+  <h1>Sent!</h1>
   <p>Thanks for your interest. I have received your message and will be in touch shortly.</p>
 </div>
 
